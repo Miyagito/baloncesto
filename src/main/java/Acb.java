f23469b1afcabd41f4ae2acbdac6dff483a42021
jose.zambrano <jomiguel60@gmail.com>
2024-02-18 00:07:48 +0100

    wip fixing sonar errors in qa

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,19 +13,26 @@ public class Acb extends HttpServlet {
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession s = req.getSession(true);
-        String nombreP = (String) req.getParameter("txtNombre");
-        String nombre = (String) req.getParameter("R1");
-        if (nombre.equals("Otros")) {
-            nombre = (String) req.getParameter("txtOtros");
-        }
-        if (bd.existeJugador(nombre)) {
-            bd.actualizarJugador(nombre);
+        String accion = req.getParameter("accion");
+
+        if ("PonerVotosACero".equals(accion)) {
+            bd.resetearVotos();
+            res.sendRedirect(res.encodeRedirectURL("index.html"));
         } else {
-            bd.insertarJugador(nombre);
+            String nombreP = req.getParameter("txtNombre");
+            String nombre = req.getParameter("R1");
+            if ("Otros".equals(nombre)) {
+                nombre = req.getParameter("txtOtros");
+            }
+            if (bd.existeJugador(nombre)) {
+                bd.actualizarJugador(nombre);
+            } else {
+                bd.insertarJugador(nombre);
+            }
+            s.setAttribute("nombreCliente", nombreP);
+            // Llamada a la página jsp que nos da las gracias
+            res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
         }
-        s.setAttribute("nombreCliente", nombreP);
-        // Llamada a la página jsp que nos da las gracias
-        res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
     }
 
     public void destroy() {
